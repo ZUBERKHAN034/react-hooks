6b527034fe3c61605b558aeb2f32ed57b7ce4707
Zuber Khan <zuberkhan034@gmail.com>
2023-02-01 00:58:34 +0530

    learning hooks

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+// import UseState from './components/UseState';
+// import UseEffectOne from './components/UseEffectOne';
+// import UseEffectTwo from './components/UseEffectTwo';
+import UseEffectThree from './components/UseEffectThree';
+function App() {
+  return (
+<>
+{/* <UseState/> */}
+{/* <UseEffectOne/> */}
+{/* <UseEffectTwo/> */}
+<UseEffectThree/>
+</>
+  );
+}
+
+export default App;
